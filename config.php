aa7eb4f696b2c27ee9375362c3804b12f787b6fa
Ian Lomas <you@example.com>
2026-05-01 13:01:35 -0400

    auto=deploy

--- config.php
+++ config.php
@@ -260,6 +260,7 @@ function mysqli_fetch_assoc($result) {
  * @return int Number of rows
  */
 function mysqli_num_rows($result) {
+    print_r($result, true);
     if (!$result || !($result instanceof PDOResultWrapper)) {
         return 0;
     }
